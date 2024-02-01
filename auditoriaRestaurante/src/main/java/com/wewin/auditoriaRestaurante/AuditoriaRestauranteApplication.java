d1361c34824a85e2685b11e11fdcd8615b6661d2
wewineitor <edwin.avilag1999@gmail.com>
2024-01-31 22:38:47 -0600

    Agregado docker-compose y cambios en el motor de bd

---
+++ auditoriaRestaurante/src/main/java/com/wewin/auditoriaRestaurante/AuditoriaRestauranteApplication.java
@@ -0,0 +1,12 @@
+package com.wewin.auditoriaRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditoriaRestauranteApplication.class, args);
+	}
+}
